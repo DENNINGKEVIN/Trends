bfbfbf5eacc43fe2f5a9e1f83503ae5b4560e0df
denningkevin <denningkev@gmail.com>
2019-11-04 08:29:17 +0300

    add required components

--- SECOND/project/github search/searcher/src/app/app.module.ts
+++ SECOND/project/github search/searcher/src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GithubComponent } from './github/github.component';
 import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { RepositoryDetailsComponent } from './repository-details/repository-details.component';
+import { UserDetailsComponent } from './user-details/user-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GithubComponent,
-    HomeComponent
+    HomeComponent,
+    NavbarComponent,
+    NotFoundComponent,
+    RepositoryDetailsComponent,
+    UserDetailsComponent
   ],
   imports: [
     BrowserModule,
