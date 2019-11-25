f709c2f5c4b061910959287471ac5fcb33e36046
denningkevin <denningkev@gmail.com>
2019-11-25 09:41:38 +0300

    edit EndangeredAnimal class and test

--- SECOND/wildlife-tracker/src/main/java/EndangeredAnimal.java
+++ SECOND/wildlife-tracker/src/main/java/EndangeredAnimal.java
@@ -1,2 +1,41 @@
+import java.util.Objects;
+
 public class EndangeredAnimal  {
+    public int id;
+    public String name;
+    public String health;
+    public int age;
+
+    public EndangeredAnimal(String name,String health,int age){
+        this.name=name;
+        this.health=health;
+        this.age=age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getHealth() {
+        return health;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        EndangeredAnimal that = (EndangeredAnimal) o;
+        return age == that.age &&
+                Objects.equals(name, that.name) &&
+                Objects.equals(health, that.health);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, health, age);
+    }
 }
--- SECOND/wildlife-tracker/src/test/java/EndangeredAnimalTest.java
+++ SECOND/wildlife-tracker/src/test/java/EndangeredAnimalTest.java
@@ -1,2 +1,33 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
 public class EndangeredAnimalTest {
+    @Test
+    public void EndangeredAnimals_instanciatesCorrectly_true(){
+        EndangeredAnimal testEndangeredAnimal=new EndangeredAnimal("Tiger","ill",2);
+        assertEquals(true,testEndangeredAnimal instanceof EndangeredAnimal);
+    }
+    @Test
+    public void EndangeredAnimals_instanciatesWithName_true(){
+        EndangeredAnimal testEndangeredAnimal=new EndangeredAnimal("Tiger","ill",2);
+        assertEquals("Tiger",testEndangeredAnimal.getName());
+    }
+    @Test
+    public void EndangeredAnimals_instanciatesWithHealth_true(){
+        EndangeredAnimal testEndangeredAnimal=new EndangeredAnimal("Tiger","ill",2);
+        assertEquals("ill",testEndangeredAnimal.getHealth());
+    }
+    @Test
+    public void EndangeredAnimals_instanciatesWithAge_true(){
+        EndangeredAnimal testEndangeredAnimal=new EndangeredAnimal("Tiger","ill",2);
+        assertEquals(2,testEndangeredAnimal.getAge());
+    }
+    @Test
+    public void EndangeredAnimals_returnIfnameIsSame_true(){
+        EndangeredAnimal testEndangeredAnimal=new EndangeredAnimal("Tiger","ill",2);
+        EndangeredAnimal testEndangeredAnimal1=new EndangeredAnimal("Tiger","ill",2);
+        assertTrue(testEndangeredAnimal.equals(testEndangeredAnimal1));
+    }
+
+
 }
