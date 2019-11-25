ad29fa7c33d2a739ea4af024e1999bff8fcbb1f4
denningkevin <denningkev@gmail.com>
2019-11-25 09:28:50 +0300

    edit Animal class and test

--- SECOND/wildlife-tracker/src/main/java/Animal.java
+++ SECOND/wildlife-tracker/src/main/java/Animal.java
@@ -1,2 +1,31 @@
+import java.util.Objects;
+
 public class Animal {
+ public int id;
+ public String name;
+
+ public Animal(String name){
+    this.name=name;
+ }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Animal animal = (Animal) o;
+        return Objects.equals(name, animal.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
 }
