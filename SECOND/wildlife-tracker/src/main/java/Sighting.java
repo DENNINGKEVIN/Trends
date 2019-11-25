3e71f5a58fde8fda8b2a97de42d50be73a744f36
denningkevin <denningkev@gmail.com>
2019-11-25 12:40:28 +0300

    edit sighting.java

--- SECOND/wildlife-tracker/src/main/java/Sighting.java
+++ SECOND/wildlife-tracker/src/main/java/Sighting.java
@@ -1,3 +1,9 @@
+import org.sql2o.Connection;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Sighting {
     private String location;
     private String rangerName;
@@ -26,4 +32,49 @@ public class Sighting {
     public int getAnimalId() {
         return animalId;
     }
+    public void delete() {
+        try(Connection con = DB.sql2o.open()) {
+            String sql = "DELETE FROM sightings WHERE id = :id;";
+            con.createQuery(sql)
+                    .addParameter("id", this.id)
+                    .executeUpdate();
+        }
+    }
+    public static List<Sighting> all() {
+        String sql = "SELECT * FROM sightings";
+        try(Connection con = DB.sql2o.open()) {
+            return con.createQuery(sql).executeAndFetch(Sighting.class);
+        }
+    }
+    public static Sighting find(int id) {
+        try(Connection con = DB.sql2o.open()) {
+            String sql = "SELECT * FROM persons where id=:id";
+            Sighting sighting = con.createQuery(sql)
+                    .addParameter("id", id)
+                    .executeAndFetchFirst(Sighting.class);
+            return sighting;
+        }
+    }
+    public List<Object> getAnimals() {
+        List<Object> allAnimals = new ArrayList<Object>();
+
+        try(Connection con = DB.sql2o.open()) {
+            String sqlAnimal = "SELECT * FROM monsters WHERE personId=:id";
+            List<Animal> animals = con.createQuery(sqlAnimal)
+                    .addParameter("id", this.id)
+                    .throwOnMappingFailure(false)
+                    .executeAndFetch(Animal.class);
+            allAnimals.addAll(animals);
+
+            String sqlEndangeredAnimal = "SELECT * FROM monsters WHERE personId=:id AND type='endangered';";
+            List<EndangeredAnimal> endangeredAnimals = con.createQuery(sqlEndangeredAnimal)
+                    .addParameter("id", this.id)
+                    .throwOnMappingFailure(false)
+                    .executeAndFetch(EndangeredAnimal.class);
+            allAnimals.addAll(endangeredAnimals);
+        }
+
+        return allAnimals;
+    }
+
 }
--- SECOND/wildlife-tracker/src/test/java/SightingsTest.java
+++ SECOND/wildlife-tracker/src/test/java/SightingsTest.java
@@ -1,3 +1,9 @@
+import org.junit.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.*;
 public class SightingsTest {
 
 }
